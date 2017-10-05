619e6cb1ac2928b38b3af7849a6e0669156954d1
Austin Van kempen <austinvkemp@gmail.com>
2017-10-05 01:35:14 -0400

    Clear and calculate button updated.

--- app/src/main/java/com/example/drewv/geocalculator/GeoCalcActivity.java
+++ app/src/main/java/com/example/drewv/geocalculator/GeoCalcActivity.java
@@ -1,10 +1,14 @@
 package com.example.drewv.geocalculator;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.View;
+
 
 import static com.example.drewv.geocalculator.R.id.distance;
 
@@ -21,12 +25,41 @@ public class GeoCalcActivity extends AppCompatActivity {
         EditText long1 = (EditText) findViewById(R.id.long1);
         EditText long2 = (EditText) findViewById(R.id.long2);
 
+        //Calculate Button
         Button calculate = (Button) findViewById(R.id.calculate);
         calculate.setOnClickListener(v -> {
             lat1.setText("9");
+
+            // Makes the keyboard disapear when you click calculate
+            View view = this.getCurrentFocus();
+            if (view != null) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+
         });
+
+
+
+
+
+
+
+        //Clear Button
         Button clear = (Button) findViewById(R.id.clear);
         clear.setOnClickListener(v -> {
+
+
+            // Makes the keyboard dissapear when you click clear
+            View view = this.getCurrentFocus();
+            if (view != null) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+
+
+
+
             lat1.setText("");
             lat2.setText("");
             long1.setText("");
